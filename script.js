4ec09333b0b4032426ca6c572e738c0972d8643a
Richard McCormick <mccormickri38@gmail.com>
2024-05-08 00:24:16 -0400

    Add script and formatting for 16 by 16 etch a sketch grid

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 console.log("IT'S WORKING!!!!")
+
+const container = document.querySelector("#container");
+//const row = document.createElement("div");
+//const block = document.createElement("div");
+//row.classList.add("row");
+let gridSize = 16;
+
+
+function fillRow(divName){
+    for(let i = 0; i< gridSize; i++){
+        const block = document.createElement("div");
+        block.classList.add("block");
+        block.setAttribute("id", "block");
+        block.style.width = "6.25%";
+        block.addEventListener("mouseenter", () => {
+            block.classList.add("colored");
+        });
+        divName.appendChild(block);
+    }
+}
+
+
+function makeGrid(containerName){
+    for(let i = 0; i< gridSize; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        containerName.appendChild(row);
+        fillRow(row);
+    }
+}
+
+
+makeGrid(container);
+
+
